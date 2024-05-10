6ae4d91703db0b2017022a7d4a68e8fee08202e2
vito <vitovalentino1604@gmail.com>
2024-05-10 21:37:56 +0700

    bermain api

--- api.php
+++ api.php
@@ -1,65 +1,66 @@
 <?php
 include 'koneksi.php';
 
-$sql = "SELECT id_device_depok, RSSI, SNR, signalStatus, flowMeter, batteryValue, batteryStatus, timestamp FROM hasil_parsed_depok";
+$sql = "SELECT id_device_depok, RSSI, SNR, signalStatus, flowMeter, batteryValue, batteryStatus, timestamp FROM hasil_parsed_depok ORDER BY timestamp DESC";
 $queryGetAllData = mysqli_query($conn, $sql);
 
 $dataArray = array(); // Array untuk menyimpan data
 
-// Mengelompokkan data berdasarkan id_device_depok dan tanggal
+// Mengelompokkan data berdasarkan id_device_depok
 while ($row = mysqli_fetch_assoc($queryGetAllData)) {
     $id_device_depok = $row['id_device_depok'];
-    $tanggal = date('Y-m-d', strtotime($row['timestamp'])); // Ambil tanggal dari timestamp
-    
-    // Buat kunci pengelompokkan berdasarkan id_device_depok dan tanggal
-    $groupKey = $id_device_depok . '_' . $tanggal;
-    
-    if (!isset($dataArray[$groupKey])) {
-        $dataArray[$groupKey] = array(
+
+    // Buat kunci pengelompokkan berdasarkan id_device_depok
+    if (!isset($dataArray[$id_device_depok])) {
+        $dataArray[$id_device_depok] = array(
             'id_device_depok' => $id_device_depok,
-            'tanggal' => $tanggal,
             'data' => array(),
             'rateDataFlow' => 0
         );
     }
-    
-    $dataArray[$groupKey]['data'][] = $row;
+
+    // Simpan data terbaru saja untuk setiap id_device_depok
+    if (empty($dataArray[$id_device_depok]['data'])) {
+        $dataArray[$id_device_depok]['data'] = $row;
+    }
 }
 
-// Menghitung rateDataFlow untuk setiap kelompok id_device_depok dan tanggal
+// Menghitung rateDataFlow untuk setiap id_device_depok
 foreach ($dataArray as &$group) {
+    $data = $group['data'];
+
+    // Mendapatkan semua data sebelumnya pada tanggal yang sama
+    $queryPrevData = "SELECT flowMeter FROM hasil_parsed_depok WHERE id_device_depok = '{$data['id_device_depok']}' AND DATE(timestamp) = DATE('{$data['timestamp']}') AND timestamp < '{$data['timestamp']}' ORDER BY timestamp DESC";
+    $resultPrevData = mysqli_query($conn, $queryPrevData);
+    
     $totalChanges = 0;
-    $totalIterations = count($group['data']);
-    
-    if ($totalIterations > 1) {
-        for ($i = 1; $i < $totalIterations; $i++) {
-            $totalChanges += ($group['data'][$i]['flowMeter'] - $group['data'][$i - 1]['flowMeter']);
-        }
-        
-        $group['rateDataFlow'] = $totalChanges * 24 / ($totalIterations - 1);
+    $totalIterations = 0;
+
+    // Perhitungan total perubahan flowMeter dan total iterasi
+    while ($prevData = mysqli_fetch_assoc($resultPrevData)) {
+        $totalChanges += ($data['flowMeter'] - $prevData['flowMeter']);
+        $totalIterations++;
+    }
+
+    if ($totalIterations > 0) {
+        // Perhitungan rateDataFlow
+        $rateDataFlow = ($totalChanges / $totalIterations) * 24;
+        $group['rateDataFlow'] = $rateDataFlow;
     } else {
-        $group['rateDataFlow'] = 0; // Jika hanya ada satu data, rateDataFlow diatur menjadi 0
+        // Jika tidak ada data sebelumnya, rateDataFlow diatur menjadi 0
+        $group['rateDataFlow'] = 0;
     }
-    
-    //Mengubah array ke format JSON
-   // $jsonOutput = json_encode($dataArray);
 
-    // Output hasilnya dalam format JSON
-    //echo $jsonOutput;
-    // //Output hasilnya dalam bentuk array untuk setiap kelompok
-    foreach ($group['data'] as $data) {
-        echo 'id_device_depok: ' . $data['id_device_depok'] . "<br>";
-        // echo 'RSSI: ' . $data['RSSI'] . "<br>";
-        // echo 'SNR: ' . $data['SNR'] . "<br>";
-        echo 'signalStatus:' . $data['signalStatus'] . "<br>";
-        // echo 'flowMeter: ' . $data['flowMeter'] . "<br>";
-        echo 'rateDataFlow: ' . $group['rateDataFlow'] . "<br>";
-        // echo 'batteryValue: ' . $data['batteryValue'] . "<br>";
-        echo 'batteryStatus:' . $data['batteryStatus'] . "<br>";
-        echo 'timestamp: ' . $data['timestamp'] . "<br>";
-        echo "<br>";
-
-    }
+    // Menampilkan data terbaru untuk setiap id_device_depok
+    echo 'id_device_depok: ' . $data['id_device_depok'] . "<br>";
+    //echo 'RSSI: ' . $data['RSSI'] . "<br>";
+    //echo 'SNR: ' . $data['SNR'] . "<br>";
+    echo 'signalStatus: ' . $data['signalStatus'] . "<br>";
+    //echo 'flowMeter: ' . $data['flowMeter'] . "<br>";
+    echo 'rateDataFlow: ' . $group['rateDataFlow'] . " m3/hari" . "<br>";    //echo 'batteryValue: ' . $data['batteryValue'] . "<br>";
+    echo 'batteryStatus: ' . $data['batteryStatus'] . "<br>";
+    echo 'timestamp: ' . $data['timestamp'] . "<br>";
+    echo "<br>";
 } 
-
 ?>
+    
