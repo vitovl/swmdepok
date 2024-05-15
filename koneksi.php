270f8c6d644b592fc4706ec1330b3ef595061dac
vito <vitovalentino1604@gmail.com>
2024-05-15 21:36:11 +0700

    push done

--- koneksi.php
+++ koneksi.php
@@ -1,18 +1,20 @@
 <?php
-    $host = 'localhost';
-    $user = 'root';
-    $pass = '';
-    $db = 'watermeter_db';
-    $conn = mysqli_connect($host, $user, $pass, $db);
 
-    if ($conn) {
-        mysqli_select_db($conn, $db);
-           
-    }
+require 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-    if(!$conn) {
-        echo "failed your connection database! ";
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    
+$host = $_ENV["host"];
+$user = $_ENV["user"];
+$pass = $_ENV["pass"];
+$db = $_ENV["db"];
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    echo "Failed to connect to database!";
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+mysqli_select_db($conn, $db);
 ?>
