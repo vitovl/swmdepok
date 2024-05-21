ed94c3de436ae9a856be031bd70db79441379759
vito <vitovalentino1604@gmail.com>
2024-05-21 23:06:41 +0700

    push maps location

--- lokasi/lokasiservice.php
+++ lokasi/lokasiservice.php
@@ -1,24 +1,35 @@
 <?php
 include '../koneksi.php';
 
-function getLokasiDevice(){
+function getLokasiDevice() {
+    set_time_limit(0); // Ini biasanya tidak diperlukan, hanya jika prosesnya memakan waktu lama.
     global $conn;
 
-    $queryLocate = "SELECT device_name, latitude, longitude
-                    FROM lokasi_device_depok";
-    $resultLocate = mysqli_query($conn, $queryLocate);
+    // Pastikan kolom yang di-SELECT dan di-JOIN sudah diindeks.
+    $queryLocate = "SELECT 
+        dd.serial_number, 
+        hpd.signalStatus, 
+        ldp.latitude, 
+        ldp.longitude FROM lokasi_device_depok ldp 
+        INNER JOIN device_depok dd ON dd.serial_number = ldp.device_name 
+        INNER JOIN hasil_parsed_depok hpd ON dd.id = hpd.id_device_depok";
 
+    $resultLocate = mysqli_query($conn, $queryLocate);
+    
     if ($resultLocate) {
         $data = [];
         while ($row = mysqli_fetch_assoc($resultLocate)) {
             $data[] = [
-                "device_name" => $row['device_name'],
+                "serial_number" => $row['serial_number'],
+                "signalStatus" => $row['signalStatus'],
                 "latitude" => $row['latitude'],
-                "longitude" => $row['longitude'],
+                "longitude" => $row['longitude']
             ];
         }
-
-        return json_encode($data); // Return the JSON data instead of echoing
+    
+        header("HTTP/1.0 200 OK");
+        header('Content-Type: application/json');
+        echo json_encode($data);
     } else {
         $response = [
             "status" => 500,
@@ -26,9 +37,7 @@ function getLokasiDevice(){
         ];
         header("HTTP/1.0 500 Server Error");
         header('Content-Type: application/json');
-        return json_encode($response); // Return the JSON response
+        echo json_encode($response);
     }
 }
-
-
 ?>
